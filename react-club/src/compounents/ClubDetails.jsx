55f3a0aee313f465e941a56cae87d3de0f7040b5
Jawad <jawadyossef8@gmail.com>
2025-01-16 08:09:41 +0200

    Syling the popup window in clubDetails

--- react-club/src/compounents/ClubDetails.jsx
+++ react-club/src/compounents/ClubDetails.jsx
@@ -1,40 +1,60 @@
+
 import {Link} from 'react-router-dom';
 import React, { useState } from "react";
 
 export default function ClubDetails(props) {
-  const [classes, setClasses] = useState([]);
 
-  const addClass = () => {
-    const newClass = {
-      id: Date.now(),
-      ClassImage: "", // Default empty image
-      ClassName: "New Class",
-      NumberOfParticipent: 0,
-      InstructorName: "Instructor Name",
-      ClassDescription: "Class Description",
-      ClassLocation: "Class Location",
+    const [newClass, setNewClass] = useState(
+          {
+            ClassImage: "",
+            ClassName: "",
+            NumberOfParticipent: 0,
+            InstructorName: "",
+            ClassDescription: "",
+            ClassLocation: "",
+          }
+        );
+
+    const [classes, setClasses] = useState([]);
+
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const openPopup = () => {
+      setNewClass({
+        ClassImage: "",
+        ClassName: "",
+        NumberOfParticipent: 0,
+        InstructorName: "",
+        ClassDescription: "",
+        ClassLocation: "",
+      });
+      setIsPopupOpen(true);
     };
-    setClasses([...classes, newClass]);
-  };
 
-  const updateClass = (id, field, value) => {
-    setClasses(
-      classes.map((cls) =>
-        cls.id === id ? { ...cls, [field]: value } : cls
-      )
-    );
-  };
-
-  const handleImageUpload = (id, event) => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        updateClass(id, "ClassImage", reader.result);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
+    const closePopup = () => setIsPopupOpen(false);
+    
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setNewClass((prevClass) => ({ ...prevClass, [name]: value }));
+    };
+  
+    const handleImageUpload = (e) => {
+      const file = e.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          setNewClass((prevClass) => ({ ...prevClass, ClassImage: reader.result }));
+        };
+        reader.readAsDataURL(file);
+      }
+    };
+  
+    const addClass = (e) => {
+      e.preventDefault();
+      setClasses([...classes, { ...newClass, id: Date.now() }]);
+      closePopup();
+    };
+  
   return (
     <>
       <div className="back-button-cont">
@@ -54,153 +74,121 @@ export default function ClubDetails(props) {
         </div>
       </div>
 
-      <div>
       <div className="classes-Add-cont">
-        <div className="classes">
-          <p>Classes:</p>
-        </div>
-        <div className="Add-Class">
-          <button onClick={addClass}>
-            <p>Add Class</p>
-          </button>
-        </div>
+            <div className="classes">
+              <p>Classes:</p>
+            </div>
+            <div className="Add-Class">
+                  <button onClick={openPopup}>
+                  <p>Add Class</p>  
+                  </button>  
+            </div>
       </div>
 
-      <div className="class-cards-cont">
-        {classes.map((cls) => (
-          <div className="class-cardsd" key={cls.id}>
-            <div className="class-cards">
-              {cls.ClassImage ? (
-                <img src={cls.ClassImage} alt="Class" />
-              ) : (
-                <p className="placeholder-image">No Image</p>
-              )}
-              <input
-                type="file"
-                accept="image/*"
-                onChange={(e) => handleImageUpload(cls.id, e)}
-              />
-            </div>
-            <div className="class-details-cont">
-              <div className="class-details">
-                <input
-                  type="text"
-                  value={cls.ClassName}
-                  onChange={(e) =>
-                    updateClass(cls.id, "ClassName", e.target.value)
-                  }
-                  placeholder="Enter Class Name"
-                />
-                <div className="ClassDetails">
-                  <p>
-                    Number of participants:
-                    <input
-                      type="number"
-                      value={cls.NumberOfParticipent}
-                      onChange={(e) =>
-                        updateClass(cls.id, "NumberOfParticipent", e.target.value)
-                      }
-                    />
-                  </p>
-                  <p>
-                    Instructor:
-                    <input
-                      type="text"
-                      value={cls.InstructorName}
-                      onChange={(e) =>
-                        updateClass(cls.id, "InstructorName", e.target.value)
-                      }
-                    />
-                  </p>
-                  <p>
-                    Description:
-                    <textarea
-                      value={cls.ClassDescription}
-                      onChange={(e) =>
-                        updateClass(cls.id, "ClassDescription", e.target.value)
-                      }
-                    />
-                  </p>
-                  <p>
-                    Location:
-                    <input
-                      type="text"
-                      value={cls.ClassLocation}
-                      onChange={(e) =>
-                        updateClass(cls.id, "ClassLocation", e.target.value)
-                      }
-                    />
-                  </p>
-                </div>
+      <div className="classes-Add-cont">
+
+              <div className="class-cards-cont">
+                {classes.map((cls) => (
+                  <div className="class-cardsd" key={cls.id}>
+                    <div className="class-cards">
+                      {cls.ClassImage ? (
+                        <img src={cls.ClassImage} alt="Class" />
+                      ) : (
+                        <p className="placeholder-image">No Image</p>
+                      )}
+                    </div>
+                            <div className="class-details-cont">
+                                <div className="class-details">
+                                      
+                                      
+                                      <div className="ClassDetails">
+                                              <h2>Class Name: {cls.ClassName}</h2>
+                                              <p>Participants: {cls.NumberOfParticipent}</p>
+                                              <p>Instructor: {cls.InstructorName}</p>
+                                              <p>Description: {cls.ClassDescription}</p>
+                                              <p>Location: {cls.ClassLocation}</p>
+                                      </div>
+                                </div>
+                       
+                            </div>
+                  </div>
+                ))}
               </div>
-            </div>
-          </div>
-        ))}
       </div>
-    </div> 
+
+      {
+        isPopupOpen && 
+                        (
+                          <div className="openPopup">
+                          <div className="popup-content">
+                            <h3>Add Class</h3>
+                            <form onSubmit={addClass}>
+                              <label>
+                                Class Name:
+                                <input
+                                  type="text"
+                                  name="ClassName"
+                                  value={newClass.ClassName}
+                                  onChange={handleInputChange}
+                                  required
+                                />
+                              </label>
+                              <label>
+                                Participants:
+                                <input
+                                  type="number"
+                                  name="NumberOfParticipent"
+                                  value={newClass.NumberOfParticipent}
+                                  onChange={handleInputChange}
+                                />
+                              </label>
+                              <label>
+                                Instructor:
+                                <input
+                                  type="text"
+                                  name="InstructorName"
+                                  value={newClass.InstructorName}
+                                  onChange={handleInputChange}
+                                  required
+                                />
+                              </label>
+                              <label>
+                                Description:
+                                <textarea
+                                  name="ClassDescription"
+                                  value={newClass.ClassDescription}
+                                  onChange={handleInputChange}
+                                />
+                              </label>
+                              <label>
+                                Location:
+                                <input
+                                  type="text"
+                                  name="ClassLocation"
+                                  value={newClass.ClassLocation}
+                                  onChange={handleInputChange}
+                                  required
+                                />
+                              </label>
+                              <label>
+                                Image:
+                                <input
+                                  type="file"
+                                  accept="image/*"
+                                  onChange={handleImageUpload}
+                                />
+                              </label>
+                              <div className="popup-buttons">
+                                <button type="submit">Add</button>
+                                <button type="button" onClick={closePopup}>
+                                  Cancel
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                        )
+      }
     </>
   );
 }
-
-
-
-
-// import {Link} from 'react-router-dom';
-
-// export default function ClubDetails(props) {
-  
-//   return (
-//     <>
-//       <div className="back-button-cont">
-//         <div className="back-button">
-//             <button>
-//                 <Link to="/ClubPage" className="back-button-link">
-//                 <i className="fas fa-arrow-left"></i>
-//                 <p>View Clubs</p>
-//                 </Link>
-//             </button>
-//         </div>
-//       </div>
-
-//       <div className="club-name-cont">
-//         <div className="club-name">
-//           <p>{props.ClubName}</p>
-//         </div>
-//       </div>
-
-//       <div className="classes-Add-cont">
-//             <div className="classes">
-//               <p>Classes:</p>
-//             </div>
-//             <div className="Add-Class">
-//                   <button>
-//                   <p>Add Class</p>  
-//                   </button>  
-//             </div>
-//       </div>
-
-//       <div className="class-cards-cont">
-        
-//         <div className="class-cardsd">
-//           <div className="class-cards">
-//                 <img src={props.ClassImage} alt="img"/>
-//           </div>
-//           <div className="class-details-cont">
-//             <div className="class-details">
-//                   <p>{props.ClassName}</p>
-                  
-//                   <div className="ClassDetails">
-//                           <p>Number of participent:{props.NumberOfParticipent}</p>
-//                           <p>Instructor: {props.InstructorName}</p>
-//                           <p>Description: {props.ClassDescription}</p>
-//                           <p>Location: {props.ClassLocation}</p>
-//                   </div>
-//             </div>
-//           </div>
-//         </div>
-        
-//       </div>
-      
-//     </>
-//   );
-// }
