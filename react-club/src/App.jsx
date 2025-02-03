551818345341f8a49f3b6ec74d1ced9c4daba8ce
Jawad <jawadyossef8@gmail.com>
2025-02-03 23:12:39 +0200

    hosting again4

--- react-club/src/App.jsx
+++ react-club/src/App.jsx
@@ -1,5 +1,12 @@
 import { useState } from 'react'
 
+import '/css/home.css';
+import '/css/top.css';
+import '/css/contacts.css';
+import '/css/club.css';
+import '/css/contact-table.css';
+import '/css/Popup.css';
+import '/css/ClubDetails.css';
 
 import './App.css'
 import Top from './compounents/Top.jsx';
